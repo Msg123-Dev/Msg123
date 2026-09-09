8089a29ac3740fa3fd58e9599fa78c76ece50c1a
Yosuke Miura <158375945+yo-miura@users.noreply.github.com>
2026-09-09 18:21:09 +0900

    Skip the perturbed evaluation when deri_type is 1

--- src/Calc_parameter.f90
+++ src/Calc_parameter.f90
@@ -18,7 +18,6 @@ module calc_parameter
   !*********************************************************************************************
     ! -- modules
     use kind_module, only: I4
-    use constval_module, only: DHALF, DTWO
     use initial_module, only: st_schm
     use make_cell, only: st_geom
     use set_condition, only: st_hydr
@@ -34,51 +33,35 @@ module calc_parameter
     ! -- local
     integer(I4) :: i
     real(DP) :: retm, beta, theta, kr, se, stor_val, srat_val
-    real(DP) :: per_phead, phead0
-    real(DP) :: kr_phead, kr_beta, kr_se, kr_term
+    real(DP) :: per_phead, phead0, kr_phead, dkr
     real(DP) :: pors_val, resi_val, dtheta_dpsi
     real(DP) :: kr_lin, kr_grad
     !-------------------------------------------------------------------------------------------
     phead0 = -st_schm%krlin_head
 
-    !$omp parallel do private(i, per_phead, retm, beta, theta, kr, se, stor_val, kr_phead) &
-    !$omp             private(kr_beta, kr_se, kr_term, pors_val, resi_val, dtheta_dpsi) &
-    !$omp             private(srat_val, kr_lin, kr_grad)
+    !$omp parallel do private(i, pors_val, resi_val, retm, per_phead, kr_lin, kr_grad, dkr) &
+    !$omp             private(beta, se, theta, srat_val, stor_val, dtheta_dpsi, kr_phead, kr)
     do i = 1, num
       pors_val = st_hydr%read_pors(i) ; resi_val = st_hydr%read_resi(i)
+      retm = DONE - DONE/st_hydr%read_vann(i)
       per_phead = pres(i) - st_geom%cell_top(i) + pertur
       kr_lin = DONE ; kr_grad = DZERO
       ! -- Relative permeability and its slope at the linear bridge head
       if (st_schm%krlin_head > DZERO) then
-        retm = DONE - DONE/st_hydr%read_vann(i)
-        beta = abs(phead0*st_hydr%read_vana(i))**st_hydr%read_vann(i)
-        se = (DONE+beta)**(-retm)
-        kr_term = DONE-(DONE-se**(DONE/retm))**retm
-        kr_lin = se**(DHALF)*kr_term**DTWO
+        call calc_kr_vgm(phead0, st_hydr%read_vana(i), st_hydr%read_vann(i), retm, kr_lin, dkr)
         kr_grad = (DONE-kr_lin)/st_schm%krlin_head
       end if
+
+      ! -- Storage and saturation ratio at the cell top
       if (per_phead < DZERO) then
-        retm = DONE - DONE/st_hydr%read_vann(i)
         beta = abs(per_phead*st_hydr%read_vana(i))**st_hydr%read_vann(i)
-        theta = resi_val + (pors_val-resi_val)*((DONE+beta))**(-retm)
+        se = (DONE+beta)**(-retm)
+        theta = resi_val + (pors_val-resi_val)*se
         srat_val = theta/pors_val
         if (st_schm%stor_type == 1) then
           stor_val = theta + st_hydr%read_spst(i)*per_phead*srat_val
         else
           stor_val = theta
-        end if
-        se = (DONE+beta)**(-retm)
-        kr_term = DONE-(DONE-se**(DONE/retm))**retm
-        kr = se**(DHALF)*kr_term**DTWO
-        if (present(dkr_dpsi)) then
-          dkr_dpsi(i) = -(retm*st_hydr%read_vann(i)/((DONE+beta)*per_phead)) *sqrt(se)*kr_term&
-                        *(DHALF*kr_term*beta + DTWO*(DONE-kr_term))
-        end if
-        if (per_phead > phead0) then
-          kr = kr_lin + (per_phead-phead0)*kr_grad
-          if (present(dkr_dpsi)) then
-            dkr_dpsi(i) = kr_grad
-          end if
         end if
         if (present(dstor_dpsi)) then
           dtheta_dpsi = (pors_val-resi_val)*se*retm*st_hydr%read_vann(i)*beta&
@@ -92,43 +75,30 @@ module calc_parameter
         end if
       else
         srat_val = DONE
-        kr = DONE
         stor_val = pors_val + st_hydr%read_spst(i)*per_phead
-        if (present(dkr_dpsi)) then
-          dkr_dpsi(i) = DZERO
-        end if
         if (present(dstor_dpsi)) then
           dstor_dpsi(i) = st_hydr%read_spst(i)
         end if
       end if
 
+      ! -- Relative permeability at the head position given by krpos_type
       if (st_schm%krpos_type == 1) then
         kr_phead = pres(i) - st_geom%cell_cent(i) + pertur
-        if (kr_phead < DZERO) then
-          retm = DONE - DONE/st_hydr%read_vann(i)
-          kr_beta = abs(kr_phead*st_hydr%read_vana(i))**st_hydr%read_vann(i)
-          kr_se = (DONE+kr_beta)**(-retm)
-          kr_term = DONE-(DONE-kr_se**(DONE/retm))**retm
-          kr = kr_se**(DHALF)*kr_term**DTWO
-          if (present(dkr_dpsi)) then
-            dkr_dpsi(i) = -(retm*st_hydr%read_vann(i)/((DONE+kr_beta)*kr_phead))*sqrt(kr_se)&
-                          *kr_term*(DHALF*kr_term*kr_beta + DTWO*(DONE-kr_term))
-          end if
-          if (kr_phead > phead0) then
-            kr = kr_lin + (kr_phead-phead0)*kr_grad
-            if (present(dkr_dpsi)) then
-              dkr_dpsi(i) = kr_grad
-            end if
-          end if
-        else
-          kr = DONE
-          if (present(dkr_dpsi)) then
-            dkr_dpsi(i) = DZERO
-          end if
-        end if
+      else
+        kr_phead = per_phead
+      end if
+      if (kr_phead >= DZERO) then
+        kr = DONE ; dkr = DZERO
+      else if (kr_phead > phead0) then
+        kr = kr_lin + (kr_phead-phead0)*kr_grad ; dkr = kr_grad
+      else
+        call calc_kr_vgm(kr_phead, st_hydr%read_vana(i), st_hydr%read_vann(i), retm, kr, dkr)
       end if
 
       rperm(i) = kr
+      if (present(dkr_dpsi)) then
+        dkr_dpsi(i) = dkr
+      end if
       if (present(srat)) then
         srat(i) = srat_val
       end if
@@ -139,6 +109,28 @@ module calc_parameter
     !$omp end parallel do
 
   end subroutine calc_srat_rperm
+
+  subroutine calc_kr_vgm(phead, vana, vann, retm, kr, dkr)
+  !*********************************************************************************************
+  ! calc_kr_vgm -- Calculate relative permeability by van Genuchten and Mualem
+  !*********************************************************************************************
+    ! -- modules
+    use kind_module, only: SP
+    use constval_module, only: DHALF, DTWO
+    ! -- inout
+    real(DP), intent(in) :: phead, retm
+    real(SP), intent(in) :: vana, vann
+    real(DP), intent(out) :: kr, dkr
+    ! -- local
+    real(DP) :: beta, se, term
+    !-------------------------------------------------------------------------------------------
+    beta = abs(phead*vana)**vann
+    se = (DONE+beta)**(-retm)
+    term = DONE-(DONE-se**(DONE/retm))**retm
+    kr = se**(DHALF)*term**DTWO
+    dkr = -(retm*vann/((DONE+beta)*phead))*sqrt(se)*term*(DHALF*term*beta + DTWO*(DONE-term))
+
+  end subroutine calc_kr_vgm
 
   subroutine calc_hyd_geoharm(srato1, srato2, hyd_c1, hyd_c2, d1, d2, hyd_gh)
   !*********************************************************************************************
